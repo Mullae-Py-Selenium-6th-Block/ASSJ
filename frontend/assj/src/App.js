95dbcb9af0d3bd42c7b7d73029889bb6164b5f7f
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-01-03 20:31:16 +0900

    자치구 선택 바

--- frontend/assj/src/App.js
+++ frontend/assj/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Sidebar from "./component/Sidebar/Sidebar";
+import SelectButton from "./component/SelectButton/SelectButton";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
         <Sidebar />
+        <div style={{ display: "flex" }}>
+          <SelectButton />
+        </div>
       </div>
     </div>
   );
---
+++ frontend/assj/src/component/SelectButton/SelectButton.js
@@ -0,0 +1,69 @@
+import "./SelectButton.css";
+import React, { useState, useEffect } from "react";
+
+const SelectButton = () => {
+  let data = [
+    "은평구",
+    "서대문구",
+    "마포구",
+    "종로구",
+    "중구",
+    "용산구",
+    "강북구",
+    "도봉구",
+    "노원구",
+    "중랑구",
+    "동대문구",
+    "성동구",
+    "광진구",
+    "성북구",
+    "강서구",
+    "양천구",
+    "영등포구",
+    "동작구",
+    "구로구",
+    "금천구",
+    "관악구",
+    "서초구",
+    "강남구",
+    "송파구",
+    "강동구",
+  ];
+
+  let [btnActive, setBtnActive] = useState("");
+
+  const toggleActive = (e) => {
+    setBtnActive((current) => {
+      return e.target.value;
+    });
+  };
+
+  return (
+    <div class="select-menu">
+      <div id="map-logo">
+        <i class="fa-solid fa-map-location-dot fa-2xl"></i>
+      </div>
+      <div id="set">
+        <div class="select-box">
+          <span class="jachigu">확인하고 싶은 자치구를 선택하세요.</span>
+          <span class="ganada">(가나다 순)</span>
+        </div>
+        <div className="container">
+          {data.map((item, idx) => {
+            return (
+              <button
+                value={idx}
+                className={"btn" + (idx == btnActive ? " active" : "")}
+                onClick={toggleActive}
+              >
+                {item}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SelectButton;
