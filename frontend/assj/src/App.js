7dd8f0b8c4b53d555c6427b83b961dcf0cfada95
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-01-03 12:07:14 +0900

    프로젝트 생성,사이드바

---
+++ frontend/assj/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./component/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
