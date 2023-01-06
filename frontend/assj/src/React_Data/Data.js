a0b430babf53e002e000558166e9261c361e1b16
Bowon Choi <102291110+Choi-Bowon@users.noreply.github.com>
2023-01-06 12:15:16 +0900

    [Modify] Data.js

--- frontend/assj/src/React_Data/Data.js
+++ frontend/assj/src/React_Data/Data.js
@@ -1,58 +1,43 @@
 // Sidebar imports
-import { UilMap, UilChart } from "@iconscout/react-unicons";
-
-export const SidebarData = [
-  {
-    icon: UilMap,
-    heading: "자치구 지도",
-    to: "/",
-  },
-  {
-    icon: UilChart,
-    heading: "자치구 랭킹",
-    to: "/rank",
-  },
-];
-// Sidebar imports
 
 export const CardsData = [
-  {
-    title: "강남구",
-    color: {
-      backGround: "#f2f2f2",
-      boxShadow: "0px 5px 10px 0px #909090",
-    },
-    series: [
-      {
-        name: "강남구",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-    ],
+ {
+  title: "강남구",
+  color: {
+   backGround: "#f2f2f2",
+   boxShadow: "0px 5px 10px 0px #909090",
   },
-  {
-    title: "강남구",
-    color: {
-      backGround: "#f2f2f2",
-      boxShadow: "0px 5px 10px 0px #909090",
-    },
-    series: [
-      {
-        name: "강남구",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-    ],
+  series: [
+   {
+    name: "강남구",
+    data: [31, 40, 28, 51, 42, 109, 100],
+   },
+  ],
+ },
+ {
+  title: "강남구",
+  color: {
+   backGround: "#f2f2f2",
+   boxShadow: "0px 5px 10px 0px #909090",
   },
-  {
-    title: "강남구",
-    color: {
-      backGround: "#f2f2f2",
-      boxShadow: "0px 5px 10px 0px #909090",
-    },
-    series: [
-      {
-        name: "강남구",
-        data: [31, 40, 28, 51, 42, 109, 100],
-      },
-    ],
+  series: [
+   {
+    name: "강남구",
+    data: [31, 40, 28, 51, 42, 109, 100],
+   },
+  ],
+ },
+ {
+  title: "강남구",
+  color: {
+   backGround: "#f2f2f2",
+   boxShadow: "0px 5px 10px 0px #909090",
   },
+  series: [
+   {
+    name: "강남구",
+    data: [31, 40, 28, 51, 42, 109, 100],
+   },
+  ],
+ },
 ];
