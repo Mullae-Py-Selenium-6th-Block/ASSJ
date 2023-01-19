cc94b0f92fc56ec4d732a886d8379e744dca4312
stillssi <109871454+Stillssi@users.noreply.github.com>
2023-01-19 16:57:26 +0900

    kakaomap 크기 조정

--- frontend/assj/src/component/kakaomap/mapContainer.js
+++ frontend/assj/src/component/kakaomap/mapContainer.js
@@ -27797,7 +27797,7 @@ function MapContainer({ selectedGu, setSelectedGu }) {
         style={{
           // 지도의 크기
           width: "100%",
-          height: "530px",
+          height: "600px",
         }}
         level={9} // 지도의 확대 레벨
         draggable={false}
